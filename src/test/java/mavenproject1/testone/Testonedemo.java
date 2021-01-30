b3c4b826310765718f8868467c6b3b0f11b3220b
dineshkumar171234 <76984678+dineshkumar171234@users.noreply.github.com>
2021-01-30 16:51:32 +0530

    Update Testonedemo.java

--- src/test/java/mavenproject1/testone/Testonedemo.java
+++ src/test/java/mavenproject1/testone/Testonedemo.java
@@ -15,7 +15,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Testonedemo {
 WebDriver driver;
 	@Test
-	public static void demo() {
+	public void demo() {
 		
 		
 		WebDriverManager.chromedriver().setup();
